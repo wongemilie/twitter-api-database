eda585eaf14f0045c656c34dd5dec03ff8c78fcf
EmilieWong <wongemilie@gmail.com>
2020-12-17 18:05:50 +0100

    Configure database and adapt code to use it

--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -1,6 +1,7 @@
 from flask_restx import Namespace, Resource, fields
-from app.db import tweet_repository
 from app.models import Tweet
+
+from app import db
 
 api = Namespace('tweets')  # Base route
 
@@ -14,52 +15,59 @@ json_new_tweet = api.model('New tweet', {
     'text': fields.String(required=True, min_length=1),  # Don't allow empty string
 })
 
-@api.route('/<int:tweet_id>')  # route extension (ie: /tweets/<int:id>)
+@api.route('/<int:id>')  # route extension (ie: /tweets/<int:id>)
 @api.response(404, 'Tweet not found')
-@api.param('tweet_id', 'The tweet unique identifier')
+@api.param('id', 'The tweet unique identifier')
 class TweetResource(Resource):
-    @api.marshal_with(json_tweet)  # Used to control JSON response format
-    def get(self, tweet_id):  # GET method
-        tweet = tweet_repository.get(tweet_id)
+    @api.marshal_with(json_tweet)
+    def get(self, id):
+        tweet = db.session.query(Tweet).get(id)
         if tweet is None:
-            api.abort(404)  # abort will throw an exception and break execution flow (equivalent to 'return' keyword for an error)
-        return tweet, 200
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            return tweet
 
     @api.marshal_with(json_tweet, code=200)
-    @api.expect(json_new_tweet, validate=True)  # Used to control JSON body format (and validate)
-    def patch(self, tweet_id):  # PATCH method
-        tweet = tweet_repository.get(tweet_id)
+    @api.expect(json_new_tweet, validate=True)
+    def patch(self, id):
+        tweet = db.session.query(Tweet).get(id)
         if tweet is None:
-            api.abort(404)
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            tweet.text = api.payload["text"]
+            db.session.commit()
+            return tweet
 
-        # body is also called payload
-        # No need to verify if 'text' is present in body, or if it is a valid string since we use validate=True
-        # body has already been validated using json_new_tweet schema
-        tweet.text = api.payload['text']
-        return None, 204
-
-    def delete(self, tweet_id):  # DELETE method
-        tweet = tweet_repository.get(tweet_id)
+    def delete(self, id):
+        tweet = db.session.query(Tweet).get(id)
         if tweet is None:
-            api.abort(404)
-        tweet_repository.remove(tweet_id)
-        return None, 204
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            db.session.delete(tweet)
+            db.session.commit()
+            return None
 
-@api.route('')  # empty route extension (ie: /tweets)
+@api.route('')
 @api.response(422, 'Invalid tweet')
 class TweetsResource(Resource):
     @api.marshal_with(json_tweet, code=201)
     @api.expect(json_new_tweet, validate=True)
-    def post(self):  # POST method
-        # No need to verify if 'text' is present in body, or if it is a valid string since we use validate=True
-        # body has already been validated using json_new_tweet schema
-        text = api.payload['text']
-        tweet = Tweet(text)
-        tweet_repository.add(tweet)
-        return tweet, 201
+    def post(self):
+        text = api.payload["text"]
+        if len(text) > 0:
+            tweet = Tweet(text=text)
+            db.session.add(tweet)
+            db.session.commit()
+            return tweet, 201
+        else:
+            return abort(422, "Tweet text can't be empty")
 
+
+@api.route('')
+@api.response(404, 'No tweets found')
+class TweetResource(Resource):
     # Here we use marshal_list_with (instead of marshal_with) to return a list of tweets
     @api.marshal_list_with(json_tweet)
     def get(self):  # GET method
-        tweets = tweet_repository.get_all()
+        tweets = db.session.query(Tweet).all()
         return tweets, 200
